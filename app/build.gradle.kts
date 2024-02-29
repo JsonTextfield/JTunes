0647d43ef3433f9c1fe2aa1a8bcabe20fdaef8b6
jason <tehblackboy@gmail.com>
2024-02-28 21:34:45 -0500

    updated gradle files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,13 +4,21 @@ plugins {
 }
 
 android {
+    signingConfigs {
+        create("release") {
+            storeFile = file("C:\\Users\\jason\\.android\\release.keystore")
+            storePassword = properties["releaseStorePassword"].toString()
+            keyAlias = properties["releaseKeyAlias"].toString()
+            keyPassword = properties["releaseKeyPassword"].toString()
+        }
+    }
     namespace = "com.jsontextfield.jtunes"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.jsontextfield.jtunes"
         minSdk = 29
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -22,11 +30,13 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
@@ -50,24 +60,57 @@ android {
 }
 
 dependencies {
+    val media3Version = "1.2.1"
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    // For media playback using ExoPlayer
+    implementation("androidx.media3:media3-exoplayer:$media3Version")
+    // For building media playback UIs
+    implementation("androidx.media3:media3-ui:$media3Version")
+    // For exposing and controlling media sessions
+    implementation("androidx.media3:media3-session:$media3Version")
+
+    // For extracting data from media containers
+    implementation("androidx.media3:media3-extractor:$media3Version")
+
+    // For integrating with Cast
+    implementation("androidx.media3:media3-cast:$media3Version")
+
+    // For scheduling background operations using Jetpack Work's WorkManager with ExoPlayer
+    implementation("androidx.media3:media3-exoplayer-workmanager:$media3Version")
+
+    // For transforming media files
+    implementation("androidx.media3:media3-transformer:$media3Version")
+
+    // Utilities for testing media components (including ExoPlayer components)
+    implementation("androidx.media3:media3-test-utils:$media3Version")
+    // Utilities for testing media components (including ExoPlayer components) via Robolectric
+    implementation("androidx.media3:media3-test-utils-robolectric:$media3Version")
+
+    // Common functionality for media database components
+    implementation("androidx.media3:media3-database:$media3Version")
+    // Common functionality for media decoders
+    implementation("androidx.media3:media3-decoder:$media3Version")
+    // Common functionality for loading data
+    implementation("androidx.media3:media3-datasource:$media3Version")
+    // Common functionality used across multiple media libraries
+    implementation("androidx.media3:media3-common:$media3Version")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.1.1")
+    implementation("androidx.compose.material3:material3:1.2.0")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("io.coil-kt:coil-compose:2.4.0")
-    implementation ("com.google.accompanist:accompanist-insets:0.14.0")
+    implementation("com.google.accompanist:accompanist-insets:0.14.0")
 
 
     // Optional - Integration with activities
-    implementation ("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.2")
     // Optional - Integration with ViewModels
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
 
     testImplementation("junit:junit:4.13.2")
