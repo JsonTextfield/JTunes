30464482fc94ec03ce76de41b638722c0699277f
jason <tehblackboy@gmail.com>
2024-02-28 21:33:33 -0500

    updated tooltip

--- app/src/main/java/com/jsontextfield/jtunes/ui/components/menu/MenuItem.kt
+++ app/src/main/java/com/jsontextfield/jtunes/ui/components/menu/MenuItem.kt
@@ -4,23 +4,40 @@ package com.jsontextfield.jtunes.ui.components.menu
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.PlainTooltip
+import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MenuItem(icon: ImageVector, tooltip: String, visible: Boolean = true, onClick: () -> Unit = {}) {
+fun MenuItem(
+    icon: ImageVector,
+    tooltip: String,
+    visible: Boolean = true,
+    onClick: () -> Unit = {}
+) {
     AnimatedVisibility(
         visible = visible,
         content = {
-            PlainTooltipBox(
-                tooltip = { Text(text = tooltip, modifier = Modifier.padding(10.dp)) },
+            TooltipBox(
+                state = rememberTooltipState(),
+                positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
+                tooltip = {
+                    PlainTooltip {
+                        Text(text = tooltip, modifier = Modifier.padding(10.dp))
+                    }
+                },
             ) {
                 IconButton(
-                    modifier = Modifier.tooltipAnchor(),
                     content = {
                         Icon(icon, tooltip)
                     },
