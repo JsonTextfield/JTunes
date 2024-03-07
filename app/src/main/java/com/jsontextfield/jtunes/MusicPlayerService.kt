ef18be59138bf9ff08a40e0b356ffba123788eb9
jason <tehblackboy@gmail.com>
2024-03-07 15:55:34 -0500

    simplified music player service

--- app/src/main/java/com/jsontextfield/jtunes/MusicPlayerService.kt
+++ app/src/main/java/com/jsontextfield/jtunes/MusicPlayerService.kt
@@ -3,8 +3,6 @@ package com.jsontextfield.jtunes
 import android.app.PendingIntent
 import android.content.Intent
 import androidx.media3.common.AudioAttributes
-import androidx.media3.common.MediaItem
-import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaSession
@@ -20,28 +18,15 @@ class MusicPlayerService : MediaSessionService() {
             .setAudioAttributes(AudioAttributes.DEFAULT, true)
             .setHandleAudioBecomingNoisy(true)
             .build()
-        mediaSession = MediaSession.Builder(this@MusicPlayerService, player).build()
-        setUpMediaSession()
-        player.addListener(object : Player.Listener {
-            override fun onMediaItemTransition(mediaItem: MediaItem?, reason: Int) {
-                super.onMediaItemTransition(mediaItem, reason)
-                setUpMediaSession()
-            }
-        })
-    }
-
-    private fun setUpMediaSession() {
-        mediaSession?.setSessionActivity(
-            PendingIntent.getActivity(
-                this@MusicPlayerService,
-                1,
-                Intent(this@MusicPlayerService, MainActivity::class.java).apply {
-                    putExtra("song", mediaSession?.player?.currentMediaItemIndex)
-                    putExtra("fromService", true)
-                },
-                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-            )
-        )
+        mediaSession = MediaSession.Builder(this@MusicPlayerService, player)
+            .setSessionActivity(
+                PendingIntent.getActivity(
+                    this@MusicPlayerService,
+                    1,
+                    Intent(this@MusicPlayerService, MainActivity::class.java),
+                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                )
+            ).build()
     }
 
     // The user dismissed the app from the recent tasks
