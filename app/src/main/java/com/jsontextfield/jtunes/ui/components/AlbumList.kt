937e9f31d2f011ae3cf7963e4ff24a184afd7bbd
jason <tehblackboy@gmail.com>
2024-04-27 00:18:07 -0400

    added a list view for albums

--- app/src/main/java/com/jsontextfield/jtunes/ui/components/AlbumList.kt
+++ app/src/main/java/com/jsontextfield/jtunes/ui/components/AlbumList.kt
@@ -3,14 +3,17 @@ package com.jsontextfield.jtunes.ui.components
 import android.content.ContentUris
 import android.graphics.Bitmap
 import android.provider.MediaStore
-import android.util.Size
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -20,54 +23,104 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.jsontextfield.jtunes.AlbumSortMode
+import com.jsontextfield.jtunes.MusicLibrary
 import com.jsontextfield.jtunes.R
 import com.jsontextfield.jtunes.entities.Album
-import java.io.FileNotFoundException
 
 @Composable
-fun AlbumList(albums: List<Album>, onItemClick: (album: Album) -> Unit = {}) {
+fun AlbumList(
+    albums: List<Album>,
+    listState: LazyListState,
+    modifier: Modifier = Modifier,
+    onItemClick: (album: Album) -> Unit = {},
+    showAsList: Boolean = true,
+    sortMode: AlbumSortMode = AlbumSortMode.Title,
+) {
     val context = LocalContext.current
-    LazyVerticalGrid(
-        columns = GridCells.Adaptive(120.dp),
-        contentPadding = PaddingValues(10.dp),
-        verticalArrangement = Arrangement.spacedBy(10.dp),
-        horizontalArrangement = Arrangement.spacedBy(10.dp),
-        content = {
-            items(albums, { album -> album.hashCode() }) { album ->
-                var bitmap: Bitmap? by remember { mutableStateOf(null) }
-                LaunchedEffect(album) {
-                    bitmap = try {
-                        val trackUri = ContentUris.withAppendedId(
+    if (showAsList) {
+        Row {
+            SectionIndex(
+                data = albums.map { album ->
+                    if (sortMode == AlbumSortMode.Title) album.title
+                    else album.artist
+                },
+                listState = listState,
+                selectedColour = colorResource(R.color.colourAccent)
+            )
+            LazyColumn(
+                state = listState,
+                modifier = modifier,
+            ) {
+                items(albums, { it.hashCode() }) { album ->
+                    ListTile(
+                        title = album.title,
+                        subtitle = "${album.artist} • " + pluralStringResource(
+                            R.plurals.songs,
+                            MusicLibrary.getInstance().songs.count { it.album == album.title },
+                            MusicLibrary.getInstance().songs.count { it.album == album.title }),
+                        leading = {
+                            var bitmap: Bitmap? by remember { mutableStateOf(null) }
+                            LaunchedEffect(album) {
+                                val uri = ContentUris.withAppendedId(
+                                    MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI,
+                                    album.id
+                                )
+                                bitmap = getCoverArt(context, uri)
+                            }
+                            CoverArtSmall(bitmap)
+                        },
+                        onClick = { onItemClick(album) },
+                    )
+                }
+                item {
+                    Text(
+                        pluralStringResource(R.plurals.albums, albums.size, albums.size),
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
+    }
+    else {
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(120.dp),
+            contentPadding = PaddingValues(10.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            horizontalArrangement = Arrangement.spacedBy(10.dp),
+            content = {
+                items(albums, { album -> album.hashCode() }) { album ->
+                    var bitmap: Bitmap? by remember { mutableStateOf(null) }
+                    LaunchedEffect(album) {
+                        val uri = ContentUris.withAppendedId(
                             MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI,
                             album.id
                         )
-                        context.contentResolver.loadThumbnail(
-                            trackUri,
-                            Size(512, 512),
-                            null
-                        )
-                    } catch (e: FileNotFoundException) {
-                        null
+                        bitmap = getCoverArt(context, uri)
                     }
+                    GalleryTile(
+                        title = "${album.title}\n${album.artist}",
+                        bitmap = bitmap,
+                        onClick = { onItemClick(album) }
+                    )
                 }
-                GalleryTile(
-                    title = "${album.title}\n${album.artist}",
-                    bitmap = bitmap,
-                    onClick = { onItemClick(album) }
-                )
-            }
-            item {
-                Text(
-                    pluralStringResource(R.plurals.albums, albums.size, albums.size),
-                    modifier = Modifier
-                        .padding(10.dp)
-                        .fillMaxWidth(),
-                    textAlign = TextAlign.Center
-                )
-            }
-        },
-    )
+                item {
+                    Text(
+                        pluralStringResource(R.plurals.albums, albums.size, albums.size),
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            },
+        )
+    }
 }
