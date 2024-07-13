5a6db5c98005c453119a3fb3d365e11b754c80fb
jason <tehblackboy@gmail.com>
2024-07-13 17:25:08 -0400

    refactoring in MainScreen

--- app/src/main/java/com/jsontextfield/jtunes/ui/components/MainScreen.kt
+++ app/src/main/java/com/jsontextfield/jtunes/ui/components/MainScreen.kt
@@ -7,7 +7,12 @@ import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.QueueMusic
 import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Album
+import androidx.compose.material.icons.rounded.Brush
+import androidx.compose.material.icons.rounded.MusicNote
+import androidx.compose.material.icons.rounded.Person
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -22,41 +27,72 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.jsontextfield.jtunes.MusicViewModel
-import com.jsontextfield.jtunes.PageState
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.jsontextfield.jtunes.R
 import com.jsontextfield.jtunes.entities.Song
 import com.jsontextfield.jtunes.ui.components.menu.Action
+import com.jsontextfield.jtunes.ui.viewmodels.MusicViewModel
+import com.jsontextfield.jtunes.ui.viewmodels.PageState
 import kotlinx.coroutines.delay
 
 @Composable
-fun MainScreen(
-    musicViewModel: MusicViewModel,
-    actions: List<Action> = ArrayList(),
-    onPlayerAction: (PlayerButton) -> Unit = {}
-) {
+fun MainScreen(musicViewModel: MusicViewModel = viewModel()) {
     val pageState by musicViewModel.pageState.collectAsState()
-    val isPlaying by musicViewModel.isPlaying.collectAsState()
+    val musicState by musicViewModel.musicState.collectAsState()
     var showNowPlayingScreen by remember { mutableStateOf(false) }
     var showQueue by remember { mutableStateOf(false) }
 
+    val actions: List<Action> = listOf(
+        Action(
+            tooltip = stringResource(id = R.string.songs),
+            icon = Icons.Rounded.MusicNote,
+            isChecked = pageState == PageState.SONGS,
+            onClick = { musicViewModel.onPageChanged(PageState.SONGS) },
+        ),
+        Action(
+            tooltip = stringResource(id = R.string.albums),
+            icon = Icons.Rounded.Album,
+            isChecked = pageState == PageState.ALBUMS,
+            onClick = { musicViewModel.onPageChanged(PageState.ALBUMS) },
+        ),
+        Action(
+            tooltip = stringResource(id = R.string.artists),
+            icon = Icons.Rounded.Person,
+            isChecked = pageState == PageState.ARTISTS,
+            onClick = { musicViewModel.onPageChanged(PageState.ARTISTS) },
+        ),
+        Action(
+            tooltip = stringResource(id = R.string.genres),
+            icon = Icons.Rounded.Brush,
+            isChecked = pageState == PageState.GENRES,
+            onClick = { musicViewModel.onPageChanged(PageState.GENRES) },
+        ),
+        Action(
+            tooltip = stringResource(id = R.string.playlists),
+            icon = Icons.AutoMirrored.Rounded.QueueMusic,
+            isChecked = pageState == PageState.PLAYLISTS,
+            onClick = { musicViewModel.onPageChanged(PageState.PLAYLISTS) },
+        ),
+    )
     Scaffold(
         topBar = { MainTopAppBar(actions = actions) },
         bottomBar = {
-            Surface(tonalElevation = 2.dp, shadowElevation = 10.dp) {
-                NowPlayingSmall(
-                    musicViewModel = musicViewModel,
-                    onPlayerAction = onPlayerAction,
-                    onClick = { showNowPlayingScreen = true },
-                )
+            musicState.currentSong?.let {
+                Surface(tonalElevation = 2.dp, shadowElevation = 10.dp) {
+                    NowPlayingSmall(
+                        musicViewModel = musicViewModel,
+                        onClick = { showNowPlayingScreen = true },
+                    )
+                }
             }
         },
         floatingActionButton = {
             if (pageState == PageState.PLAYLISTS) {
                 FloatingActionButton(
                     containerColor = MaterialTheme.colorScheme.primary,
-                    contentColor = Color.White,
+                    contentColor = MaterialTheme.colorScheme.onPrimary,
                     onClick = {
                         // create new playlist
                     },
@@ -86,48 +122,26 @@ fun MainScreen(
         enter = slideInVertically { it },
         exit = slideOutVertically { it },
     ) {
-        var progress by remember {
-            mutableLongStateOf(
-                musicViewModel.mediaController?.currentPosition ?: 0L
-            )
-        }
-        LaunchedEffect(isPlaying) {
-            while (isPlaying) {
-                musicViewModel.mediaController?.let {
-                    progress = it.currentPosition
-                }
+        var progress by remember { mutableLongStateOf(musicViewModel.currentPlayerPosition) }
+        LaunchedEffect(musicState.isPlaying) {
+            while (musicState.isPlaying) {
+                progress = musicViewModel.currentPlayerPosition
                 delay(100)
             }
         }
         NowPlayingLarge(
             musicViewModel = musicViewModel,
             onBackPressed = { showNowPlayingScreen = false },
-            onQueuePressed = {
-                showQueue = true
-            },
-            onPlayerAction = onPlayerAction,
+            onQueuePressed = { showQueue = true },
             position = progress.toFloat(),
-            onSeek = { musicViewModel.mediaController?.seekTo(it.toLong()) },
+            onSeek = { musicViewModel.seekTo(it.toLong()) },
         )
     }
-
-    AnimatedVisibility(
-        visible = showQueue,
-    ) {
-        val songs = remember { mutableListOf<Song>() }
-        LaunchedEffect(showQueue) {
-            musicViewModel.mediaController?.let {
-                for (i in 0 until it.mediaItemCount) {
-                    val mediaItem = it.getMediaItemAt(i)
-                    musicViewModel.musicLibrary.songs.find { song ->
-                        song.title == mediaItem.mediaMetadata.title
-                                && song.artist == mediaItem.mediaMetadata.artist
-                                && song.album == mediaItem.mediaMetadata.albumTitle
-                                && song.genre == mediaItem.mediaMetadata.genre
-                    }?.let { song -> songs.add(song) }
-                }
-            }
-        }
+    var songs by remember { mutableStateOf<List<Song>>(emptyList()) }
+    LaunchedEffect(showQueue) {
+        songs = musicViewModel.getQueueSongs()
+    }
+    if (showQueue) {
         QueueView(
             songs = songs,
             onBackPressed = { showQueue = false },
